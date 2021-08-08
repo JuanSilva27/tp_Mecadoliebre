79db491625dd3a1f33cb7599201c30438bde1803
JuanSilva27 <juan.silva.0270@gmail.com>
2021-08-08 18:55:10 -0300

    se creo archivo html

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app= express();
 const port=3030;
 const path= require("path")
 
-app.get("/",(req,res)=>res.send("Hola"));
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"views","home.html")));
 
 app.listen(port,()=>console.log("se levanto servidor en puerto "+ port));
