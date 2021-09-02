bad9025b9cb76f29929516016fe028b8b6d522e7
JuanSilva27 <juan.silva.0270@gmail.com>
2021-09-02 20:20:01 -0300

    se creo rama formularios

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const path= require("path")
 app.use(express.static("public"))
 
 app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"views","home.html")));
+app.get("/register",(req,res)=>res.sendFile(path.join(__dirname,"views","register.html")));
 
 app.listen(port,()=>console.log("se levanto servidor en puerto "+ port));
